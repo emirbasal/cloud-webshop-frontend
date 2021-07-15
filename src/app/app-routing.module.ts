4e9455d028fc09d9f85726a9f7bb21cb8d061dbb
hm-basal <basal@hm.edu>
2021-07-15 18:33:08 +0200

    Refactor project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartComponent } from './components/cart/cart.component';
+import { CartComponent } from './components/cart-components/cart/cart.component';
 import { CreateProductComponent } from './components/create-product/create-product.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { OrderDetailComponent } from './components/order-detail/order-detail.component';
+import { HomepageComponent } from './components/homepage-components/homepage/homepage.component';
+import { ProductDetailComponent } from './components/detail-components/product-detail/product-detail.component';
+import { OrderDetailComponent } from './components/detail-components/order-detail/order-detail.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
